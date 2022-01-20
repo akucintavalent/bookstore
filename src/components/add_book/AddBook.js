1d806cd5c75a6740b96d467b15701fc97d2629fe
Bohdan Shcherbak <bogdan.szczerbak@gmail.com>
2022-01-20 21:19:08 +0200

    Broaden list of categories & Finish styling the app

--- src/components/add_book/AddBook.js
+++ src/components/add_book/AddBook.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addData } from '../../redux/books/books';
+import './AddBook.css';
 
 const AddBook = (props) => {
   const { categories } = props;
@@ -22,16 +23,17 @@ const AddBook = (props) => {
     dispatch(addData(newBook));
   };
   return (
-    <div>
-      <h3>ADD NEW BOOK</h3>
-      <div>
-        <input type="text" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} />
-        <select name="categories" id="categories">
+    <div className="add-book-container">
+      <h3 className="add-book-title">ADD NEW BOOK</h3>
+      <div className="add-book-form-container">
+        <input className="book-title-input" type="text" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} />
+        <select className="book-category-input" name="categories" id="categories">
+          <option className="first-option" value="" disabled selected hidden>Category</option>
           {categories.map((category) => (
             <option key={category.id} value={category.name}>{category.name}</option>
           ))}
         </select>
-        <button type="button" onClick={submitBookToStore}>ADD BOOK</button>
+        <button className="add-book-button" type="button" onClick={submitBookToStore}>ADD BOOK</button>
       </div>
     </div>
   );
